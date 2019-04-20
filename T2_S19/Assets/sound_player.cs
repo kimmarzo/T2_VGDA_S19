c8ee454b230c9e0ef2c8ffe9fb32bc6f9888d8b6
knguyen4200 <knguyen1517352@gmail.com>
2019-04-20 01:03:21 -0700

    updated sounds folder. added in basic sound player script for footsteps

---
+++ T2_S19/Assets/sound_player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_player : MonoBehaviour
+{
+    AudioSource walking_source;
+    AudioClip walking_clip;
+    Vector3 initialPos;
+    Vector3 finalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walking_source = gameObject.GetComponent<AudioSource>();
+        initialPos = gameObject.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finalPos = gameObject.transform.localPosition;
+  
+
+        if (initialPos.x != finalPos.x && walking_source.isPlaying == false)
+        {
+            walking_source.Play();
+            initialPos = finalPos;
+        }
+
+        if (initialPos.y != finalPos.y && walking_source.isPlaying == false)
+        {
+            walking_source.Play();
+            initialPos = finalPos;
+
+        }
+
+        if (initialPos.z != finalPos.z && walking_source.isPlaying == false)
+        {
+            walking_source.Play();
+            initialPos = finalPos;
+        }
+    }
+
+ 
+}
