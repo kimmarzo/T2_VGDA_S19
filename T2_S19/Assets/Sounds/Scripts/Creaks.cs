e99dab5ba7c9c4ca15fd029feb0fdfbedb134019
knguyen4200 <knguyen1517352@gmail.com>
2019-04-24 13:47:01 -0700

    added in creaking script

---
+++ T2_S19/Assets/Sounds/Scripts/Creaks.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creaks : MonoBehaviour
+{
+    AudioSource creak_sound;
+    private float randomTime;
+    private float timeCounter;
+    private int steppedOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SphereCollider sphereCollider = gameObject.GetComponent<SphereCollider>();
+        sphereCollider.isTrigger = true;
+        creak_sound = GetComponent<AudioSource>();
+        randomTime = 2.0f;
+        timeCounter = 0.0f;
+        steppedOn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+        if (steppedOn == 1)
+        {
+            ResetTimer();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (creak_sound.isPlaying == false && timeCounter > randomTime)
+        {
+            steppedOn = 1;
+            creak_sound.Play();
+        }
+    }
+
+    private void ResetTimer()
+    {
+        randomTime = Random.Range(15.0f, 20.0f);
+        timeCounter = 0.0f;
+        steppedOn = 0;
+    }
+}
---
+++ T2_S19/Assets/Sounds/Scripts/Walking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound_player : MonoBehaviour
+{
+    public AudioSource walking_source;
+    Vector3 initialPos;
+    Vector3 finalPos;
+    Vector3 difference;
+    public float walkingPace = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        initialPos = GetComponent<Transform>().position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finalPos = GetComponent<Transform>().position;
+        difference = finalPos - initialPos;
+
+        if (difference.magnitude > walkingPace && walking_source.isPlaying == false)
+        {
+            walking_source.Play();
+            initialPos = finalPos;
+        }
+
+    }
+}
