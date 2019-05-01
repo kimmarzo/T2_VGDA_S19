dbb1b20e4ee0db2f6da95fe61324851efc937878
knguyen4200 <knguyen4200@yahoo.com>
2019-04-30 22:53:54 -0700

    minor volume adjusts

---
+++ T2_S19/Assets/window_sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class window_sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource sound = GetComponent<AudioSource>();
+        sound.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
