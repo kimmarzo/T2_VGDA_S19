658e941386c16eb969a2b6b63294e78c04db27cd
knguyen4200 <knguyen4200@yahoo.com>
2019-04-30 22:40:26 -0700

    basement warning sound, window distant wind sound

---
+++ T2_S19/Assets/Sounds/Scripts/PlaySoundOnce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnce : MonoBehaviour
+{
+    AudioSource sound;
+    bool alreadyPlayed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!alreadyPlayed)
+        {
+            sound.Play();
+            alreadyPlayed = true;
+            Debug.Log("Played ghost sound");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
