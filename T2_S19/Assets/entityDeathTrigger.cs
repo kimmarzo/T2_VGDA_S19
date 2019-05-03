3fa384fc90cc1eb0d5448628f49af84c8cb23c1e
kimmarzo <kjrmarzo@gmail.com>
2019-05-02 23:53:01 -0800

    ** Death feature

---
+++ T2_S19/Assets/entityDeathTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class entityDeathTrigger : MonoBehaviour
+{
+
+    private bool isInsideDeath = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+
+
+}
