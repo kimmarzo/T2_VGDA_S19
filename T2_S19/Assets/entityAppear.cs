1834f4dd0f537260139474e0969c35b3246c4462
kimmarzo <kjrmarzo@gmail.com>
2019-05-03 10:16:33 -0800

    no message

--- T2_S19/Assets/entityAppear.cs
+++ T2_S19/Assets/entityAppear.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class entityAppear : MonoBehaviour
 {
     public GameObject flashlightChecker;
-    public GameObject entity;
+    public GameObject entity1;
+    public GameObject entity2;
+    public GameObject entity3;
     public bool entityisOn = true;
 
     private void Update()
     {
         if ((flashlightChecker.GetComponent<flashlightToggle>().flashlightisOn == true))
         {
-            entity.SetActive(false);
+            entity1.SetActive(false);
+            entity2.SetActive(false);
+            entity3.SetActive(false);
         }
         else
         {
-            entity.SetActive(true);
+            entity1.SetActive(true);
+            entity2.SetActive(true);
+            entity3.SetActive(true);
         }
     }
     //private void OnTriggerEnter(Collider other)
