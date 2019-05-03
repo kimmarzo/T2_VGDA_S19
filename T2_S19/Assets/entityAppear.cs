47f430c4caa391fda05d5fcfa8a0b7cefa254975
kimmarzo <kjrmarzo@gmail.com>
2019-05-03 10:29:23 -0800

    Revert "no message"
    This reverts commit 1834f4dd0f537260139474e0969c35b3246c4462.

--- T2_S19/Assets/entityAppear.cs
+++ T2_S19/Assets/entityAppear.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 public class entityAppear : MonoBehaviour
 {
     public GameObject flashlightChecker;
-    public GameObject entity1;
-    public GameObject entity2;
-    public GameObject entity3;
+    public GameObject entity;
     public bool entityisOn = true;
 
     private void Update()
     {
         if ((flashlightChecker.GetComponent<flashlightToggle>().flashlightisOn == true))
         {
-            entity1.SetActive(false);
-            entity2.SetActive(false);
-            entity3.SetActive(false);
+            entity.SetActive(false);
         }
         else
         {
-            entity1.SetActive(true);
-            entity2.SetActive(true);
-            entity3.SetActive(true);
+            entity.SetActive(true);
         }
     }
     //private void OnTriggerEnter(Collider other)
